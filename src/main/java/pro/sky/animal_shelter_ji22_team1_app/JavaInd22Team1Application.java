37aff5232b3901a595f70c276f0c9d472e4c0679
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-08-18 22:22:51 +0300

    JavaInd22 Team1 - Animal Shelter Spring Boot Application - начальный коммит

---
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/JavaInd22Team1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.animal_shelter_ji22_team1_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInd22Team1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInd22Team1Application.class, args);
+	}
+
+}
