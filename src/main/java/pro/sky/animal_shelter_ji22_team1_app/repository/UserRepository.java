8347ab2020ed882feecb824b8829a6d1bdf7034a
Dmitriy Stroganov <distrog@gmail.com>
2024-08-24 18:58:06 +0700

    создан репозиторий юзеров

---
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package pro.sky.animal_shelter_ji22_team1_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
