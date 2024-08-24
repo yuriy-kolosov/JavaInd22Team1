f2e5de32761c10d3d046a7e96ed6c0fefb891da4
Dmitriy Stroganov <distrog@gmail.com>
2024-08-24 20:02:07 +0700

    добавил статусы к ошибкам

--- src/main/java/pro/sky/animal_shelter_ji22_team1_app/exception/UserDoesNotExistException.java
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/exception/UserDoesNotExistException.java
@@ -1,5 +1,9 @@
 package pro.sky.animal_shelter_ji22_team1_app.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserDoesNotExistException extends RuntimeException{
     public UserDoesNotExistException(String message) {
         super(message);
--- src/main/java/pro/sky/animal_shelter_ji22_team1_app/exception/UserWithThisLoginAlreadyExistException.java
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/exception/UserWithThisLoginAlreadyExistException.java
@@ -1,5 +1,9 @@
 package pro.sky.animal_shelter_ji22_team1_app.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserWithThisLoginAlreadyExistException extends RuntimeException{
     public UserWithThisLoginAlreadyExistException(String message) {
         super(message);
