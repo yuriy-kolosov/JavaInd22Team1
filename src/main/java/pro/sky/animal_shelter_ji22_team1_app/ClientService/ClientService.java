8dbc837a0397cec0c85fedcc0dd2791bcaa8d4f7
Dmitriy Stroganov <distrog@gmail.com>
2024-08-25 20:11:28 +0700

    создан телеграмБотЛистенер, создан класс конфигурации бота, Создан интерфейс по общению телеграм бота с клиентом и его имплементации, создана логика определения типа клиента по его сообщению

---
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/ClientService/ClientService.java
@@ -0,0 +1,5 @@
+package pro.sky.animal_shelter_ji22_team1_app.ClientService;
+
+public interface ClientService {
+    String getGreetingText();
+}
---
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/ClientService/ClientType.java
@@ -0,0 +1,7 @@
+package pro.sky.animal_shelter_ji22_team1_app.ClientService;
+
+public enum ClientType {
+    NEW_CLIENT,
+    CHOOSING_CLIENT,
+    ANIMAL_OWNER
+}
