bf4bc8acd8e242855df096e99474a226431d2773
Dmitriy Stroganov <distrog@gmail.com>
2024-09-01 12:45:41 +0700

    telegrambot начал реагировать н чат

--- src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
@@ -5,10 +5,13 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
 import pro.sky.animal_shelter_ji22_team1_app.menu_service.MenuService;
 
 import java.util.List;
 
+@Component
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final TelegramBot telegramBot;
@@ -19,16 +22,21 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         this.evaluator = evaluator;
     }
 
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-
-            Integer chatId = Math.toIntExact(update.message().chat().id());
-            MenuService clientService = evaluator.evaluate(chatId);
-            String textMessage = clientService.getGreetingText();
-            SendMessage message = new SendMessage(chatId, textMessage);
-            SendResponse response = telegramBot.execute(message);
-
+            if (update.message().text().equals("/start")) {
+                Integer chatId = Math.toIntExact(update.message().chat().id());
+                MenuService clientService = evaluator.evaluate(chatId);
+                String textMessage = clientService.getGreetingText();
+                SendMessage message = new SendMessage(chatId, textMessage);
+                SendResponse response = telegramBot.execute(message);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
