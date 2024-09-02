82daabec186d1e6c95ba5a768c9e4d361a708663
MariiaMerkel <mariia_merkel@mail.ru>
2024-09-02 08:28:41 +0300

    Создала сервис для работы с животными

---
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/service/AnimalService.java
@@ -0,0 +1,16 @@
+package pro.sky.animal_shelter_ji22_team1_app.service;
+
+import java.util.Collection;
+import pro.sky.animal_shelter_ji22_team1_app.entity.Animal;
+
+public interface AnimalService {
+    Collection<Animal> findAll();
+
+    Animal findById(Long animalId);
+
+    void save(Animal animal);
+
+    Animal change(Animal animal);
+
+    Animal delete(Long animalId);
+}
