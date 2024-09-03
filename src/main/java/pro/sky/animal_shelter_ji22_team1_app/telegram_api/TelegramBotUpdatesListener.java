9e4fbfb2280d3dd0ecf7e84605527b60edffc2ae
Dmitriy Stroganov <distrog@gmail.com>
2024-09-03 22:44:50 +0700

    изменил логику telegramp api

--- src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
@@ -2,12 +2,17 @@ package pro.sky.animal_shelter_ji22_team1_app.telegram_api;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.GetChatResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
+import pro.sky.animal_shelter_ji22_team1_app.entity.Type;
+import pro.sky.animal_shelter_ji22_team1_app.entity.UserEntity;
 import pro.sky.animal_shelter_ji22_team1_app.menu_service.MenuService;
+import pro.sky.animal_shelter_ji22_team1_app.repository.UserRepository;
 
 import java.util.List;
 
@@ -17,9 +22,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private final TelegramBot telegramBot;
     private final UserEvaluator evaluator;
 
-    public TelegramBotUpdatesListener(TelegramBot telegramBot, UserEvaluator evaluator) {
+    private final UserRepository userRepository;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, UserEvaluator evaluator, UserRepository userRepository) {
         this.telegramBot = telegramBot;
         this.evaluator = evaluator;
+        this.userRepository = userRepository;
     }
 
     @PostConstruct
@@ -35,7 +43,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
                 sendGreetingMessage(chatId);
 
-                sendMessageFromMenuService(chatId);
+            }
+            if (update.message().text().equals("/help")) {
+                Long chatId = update.message().chat().id();
+
+                sendHelpInformation(chatId);
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
@@ -43,14 +55,45 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private void sendGreetingMessage(Long chatId) {
 
-        String greetingText = "Доброго времени суток, я бот который помогает животным, оказавшимся в сложной ситуации, и людям, " +
-                              "которые хотят найти себе нового пушистого друга, обрести то, что они ищут";
+        String greetingText = "Доброго времени суток, я бот который помогает животным, оказавшимся в сложной ситуации, " +
+                              "и людям, которые хотят найти себе нового пушистого друга, обрести то, что они ищут. " +
+                              "Хочешь узнать больше о нас введи команду /help";
 
         SendMessage greetingMessage = new SendMessage(chatId, greetingText);
         SendResponse greetingResponse = telegramBot.execute(greetingMessage);
     }
 
-    private void sendMessageFromMenuService(Long chatId){
+    private void sendHelpInformation(Long chatId) {
+        StringBuilder infoTextBuilder = new StringBuilder("Доступны следующие команды:");
+        infoTextBuilder.append("\n/shelter - узнать информацию о приюте;");
+        UserEntity user = userRepository.findByChatId(chatId);
+        if (user == null) {
+            infoTextBuilder.append("\n/volunteer - связаться с волонтером;");
+            String infoText = infoTextBuilder.toString();
+            SendMessage infoMessage = new SendMessage(chatId, infoText);
+            SendResponse menuResponse = telegramBot.execute(infoMessage);
+            return;
+        }
+        if (user.getType().equals(Type.CHOOSING_CLIENT)) {
+            infoTextBuilder.append("\n/choosing_animal - узнать как взять животное из приюта");
+        }
+        if (user.getType().equals(Type.ANIMAL_OWNER)) {
+            infoTextBuilder.append("\n/send_report - прислать отчет;");
+        }
+        infoTextBuilder.append("\n/volunteer - связаться с волонтером;");
+//        String infoText = """
+//                Доступны следующие команды:
+//                /shelter - узнать информацию о приюте;
+//                /choosing_animal - узнать как взять животное из приюта;
+//                /send_report - прислать отчет;
+//                /volunteer - связаться с волонтером;
+//                """;
+        String infoText = infoTextBuilder.toString();
+        SendMessage infoMessage = new SendMessage(chatId, infoText);
+        SendResponse menuResponse = telegramBot.execute(infoMessage);
+    }
+
+    private void sendMessageFromMenuService(Long chatId) {
         MenuService clientService = evaluator.evaluate(chatId);
         String textMessage = clientService.getGreetingText();
         SendMessage message = new SendMessage(chatId, textMessage);
