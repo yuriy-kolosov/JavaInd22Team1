04f169773edd234f43977fc25560b5243fa5e501
Dmitriy Stroganov <distrog@gmail.com>
2024-09-05 22:40:13 +0700

    добавил паттерн команда, добавил логику сохранения пользователя

--- src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
@@ -2,31 +2,31 @@ package pro.sky.animal_shelter_ji22_team1_app.telegram_api;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.GetChatResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
+import pro.sky.animal_shelter_ji22_team1_app.command.RemoteControl;
 import pro.sky.animal_shelter_ji22_team1_app.entity.Type;
 import pro.sky.animal_shelter_ji22_team1_app.entity.UserEntity;
-import pro.sky.animal_shelter_ji22_team1_app.menu_service.MenuService;
 import pro.sky.animal_shelter_ji22_team1_app.repository.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final TelegramBot telegramBot;
-    private final UserEvaluator evaluator;
-
+    private final RemoteControl remoteControl;
     private final UserRepository userRepository;
 
-    public TelegramBotUpdatesListener(TelegramBot telegramBot, UserEvaluator evaluator, UserRepository userRepository) {
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, RemoteControl remoteControl,
+                                      UserRepository userRepository) {
         this.telegramBot = telegramBot;
-        this.evaluator = evaluator;
+        this.remoteControl = remoteControl;
         this.userRepository = userRepository;
     }
 
@@ -38,65 +38,45 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            if (update.message().text().equals("/start")) {
-                Long chatId = update.message().chat().id();
 
-                sendGreetingMessage(chatId);
+            saveUser(update);
 
-            }
-            if (update.message().text().equals("/help")) {
-                Long chatId = update.message().chat().id();
+            Long chatId = update.message().chat().id();
 
-                sendHelpInformation(chatId);
+            switch (update.message().text()) {
+                case "/start" -> sendMessage(chatId, remoteControl.start());
+                case "/help" -> sendMessage(chatId, remoteControl.help());
+                default -> sendMessage(chatId, "такой команды не существует. Вы можете вызвать справку" +
+                                               "командой /help");
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    private void sendGreetingMessage(Long chatId) {
-
-        String greetingText = "Доброго времени суток, я бот который помогает животным, оказавшимся в сложной ситуации, " +
-                              "и людям, которые хотят найти себе нового пушистого друга, обрести то, что они ищут. " +
-                              "Хочешь узнать больше о нас введи команду /help";
-
-        SendMessage greetingMessage = new SendMessage(chatId, greetingText);
-        SendResponse greetingResponse = telegramBot.execute(greetingMessage);
-    }
-
-    private void sendHelpInformation(Long chatId) {
-        StringBuilder infoTextBuilder = new StringBuilder("Доступны следующие команды:");
-        infoTextBuilder.append("\n/shelter - узнать информацию о приюте;");
-        UserEntity user = userRepository.findByChatId(chatId);
-        if (user == null) {
-            infoTextBuilder.append("\n/volunteer - связаться с волонтером;");
-            String infoText = infoTextBuilder.toString();
-            SendMessage infoMessage = new SendMessage(chatId, infoText);
-            SendResponse menuResponse = telegramBot.execute(infoMessage);
-            return;
-        }
-        if (user.getType().equals(Type.CHOOSING_CLIENT)) {
-            infoTextBuilder.append("\n/choosing_animal - узнать как взять животное из приюта");
-        }
-        if (user.getType().equals(Type.ANIMAL_OWNER)) {
-            infoTextBuilder.append("\n/send_report - прислать отчет;");
-        }
-        infoTextBuilder.append("\n/volunteer - связаться с волонтером;");
-//        String infoText = """
-//                Доступны следующие команды:
-//                /shelter - узнать информацию о приюте;
-//                /choosing_animal - узнать как взять животное из приюта;
-//                /send_report - прислать отчет;
-//                /volunteer - связаться с волонтером;
-//                """;
-        String infoText = infoTextBuilder.toString();
-        SendMessage infoMessage = new SendMessage(chatId, infoText);
-        SendResponse menuResponse = telegramBot.execute(infoMessage);
-    }
-
-    private void sendMessageFromMenuService(Long chatId) {
-        MenuService clientService = evaluator.evaluate(chatId);
-        String textMessage = clientService.getGreetingText();
-        SendMessage message = new SendMessage(chatId, textMessage);
+    private void sendMessage(Long chatId, String text) {
+        SendMessage message = new SendMessage(chatId, text);
         SendResponse response = telegramBot.execute(message);
     }
+
+    private void saveUser(Update update) {
+        if (userRepository.findByChatId(update.message().chat().id()) != null) {
+            return;
+        }
+
+        User user = update.message().from();
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setFirstname(user.firstName());
+        userEntity.setLastname(user.lastName());
+        userEntity.setChatId(update.message().chat().id());
+        userEntity.setRegistrationDate(LocalDateTime.now());
+        userEntity.setType(Type.NEW_CLIENT);
+        userEntity.setLogin(user.username());
+
+        userEntity.setSurname("asdgasdg");
+        userEntity.setPhone("asdgasg");
+
+        userRepository.save(userEntity);
+    }
+
 }
