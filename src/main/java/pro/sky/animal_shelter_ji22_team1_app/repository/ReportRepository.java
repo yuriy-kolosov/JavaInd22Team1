3d4d6ac8dce92d6b274fac0e3cad75eb68181d6b
MariiaMerkel <mariia_merkel@mail.ru>
2024-09-13 08:04:33 +0300

    Подготовила сущность, сервис и таблицу для работы с отчётами

---
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/repository/ReportRepository.java
@@ -0,0 +1,12 @@
+package pro.sky.animal_shelter_ji22_team1_app.repository;
+
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.sky.animal_shelter_ji22_team1_app.entity.ReportEntity;
+
+public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
+
+    @NotNull
+    List<ReportEntity> findAll();
+}
