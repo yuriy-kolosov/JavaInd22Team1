c039dc31d183cd90ebd38c548cae0a5f6a8b9e74
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-09-13 07:26:59 +0300

    JavaInd22 Team1 - Animal Shelter Spring Boot Application - Point 2 - Rel 3

--- src/main/java/pro/sky/animal_shelter_ji22_team1_app/command/shelter_info_menu/ShelterInfoMenu.java
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/command/shelter_info_menu/ShelterInfoMenu.java
@@ -13,37 +13,38 @@ public class ShelterInfoMenu {
     }
 
     public String shelterInfo() {
-       ShelterEntity shelter = shelterService.findFirst();
+        ShelterEntity shelter = shelterService.findFirst();
 
         return """
                 Приют "%s" расположен по адресу: %s) +
-                /location - Чтобы ознакомиться со схемой проезда к нему
-                /shelter_contacts - Контактные данные охраны и правила оформления пропуска+
-                /health_and_safety - Общие правила техники безопасности на территории приюта
-                /client_contacts - Оставить контактные данные
-                /volunteer - Связаться с волонтером
+                /location           - Чтобы ознакомиться со схемой проезда к нему;
+                /shelter_contacts   - Контактные данные охраны и правила оформления пропуска;
+                /health_and_safety  - Общие правила техники безопасности на территории приюта;
+                /client_contacts    - Оставить контактные данные;
+                /volunteer          - Связаться с волонтером
                 """
-               .formatted(shelter.getName(), shelter.getContacts());
+                .formatted(shelter.getName(), shelter.getContacts());
     }
-    public String location(){
+
+    public String location() {
         return "информация о проезде к приюту";
     }
 
-    public String shelterContacts(){
+    public String shelterContacts() {
         return shelterService.findFirst().getContacts();
     }
 
-    public String healthAndSafety(){
+    public String healthAndSafety() {
         return shelterService.findFirst().getSafety_recommendations();
     }
 
-    public String clientContacts(){
+    public String clientContacts() {
         return """
-                Давайте сохраним ваши данные для будующего сотрудничества!
-                /firstname - Ввести имя 
-                /surname - Ввесте отчество
-                /lastname - Ввести фамилию
-                /phone - Ввести контактный телефон
+                Давайте сохраним ваши данные для будущего сотрудничества:
+                /firstname  - Ввести имя;
+                /surname    - Ввести отчество;
+                /lastname   - Ввести фамилию;
+                /phone      - Ввести контактный телефон
                 """;
     }
 }
--- src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animal_shelter_ji22_team1_app/telegram_api/TelegramBotUpdatesListener.java
@@ -3,17 +3,13 @@ package pro.sky.animal_shelter_ji22_team1_app.telegram_api;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 import pro.sky.animal_shelter_ji22_team1_app.command.RemoteControl;
-import pro.sky.animal_shelter_ji22_team1_app.entity.Type;
-import pro.sky.animal_shelter_ji22_team1_app.entity.UserEntity;
-import pro.sky.animal_shelter_ji22_team1_app.repository.UserRepository;
+import pro.sky.animal_shelter_ji22_team1_app.user_safer.UserSafer;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -21,13 +17,15 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final TelegramBot telegramBot;
     private final RemoteControl remoteControl;
-    private final UserRepository userRepository;
+
+
+    private final UserSafer userSafer;
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot, RemoteControl remoteControl,
-                                      UserRepository userRepository) {
+                                      UserSafer userSafer) {
         this.telegramBot = telegramBot;
         this.remoteControl = remoteControl;
-        this.userRepository = userRepository;
+        this.userSafer = userSafer;
     }
 
     @PostConstruct
@@ -41,18 +39,33 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
             if (update.message() != null) {
 
-                saveUser(update);
+                userSafer.saveUser(update);
 
                 Long chatId = update.message().chat().id();
 
+//                switch (update.message().text()) {
+                if (update.message().text().matches("firstname\\s\\w+")) {
+                    userSafer.safeFirstname(update);
+                }
+
+                if (update.message().text().matches("surname\\s\\w+")) {
+                    userSafer.safeSurname(update);
+                }
+
+                if (update.message().text().matches("lastname\\s\\w+")) {
+                    userSafer.safeLastname(update);
+                }
+
+                if (update.message().text().matches("\\+7\\s\\d{3}\\s\\d{3}\\s\\d{2}-\\d{2}")) {
+                    userSafer.safePhone(update);
+                }
+
                 switch (update.message().text()) {
-//                                                                                          Menu Command from Point#0
+
                     case "/start" -> sendMessage(chatId, remoteControl.start());
                     case "/help" -> sendMessage(chatId, remoteControl.help());
-//                                                                                                      ...
 //                                                                                          Menu Command from Point#1
                     case "/shelter_info" -> sendMessage(chatId, remoteControl.shelterInfo());
-//                                                                                                      ...
 //                                                                                          Menu Command from Point#2
                     case "/entry" -> sendMessage(chatId, remoteControl.entry());
 //                                                                                          for choosing types of animals
@@ -75,7 +88,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                     case "/dog_handler_list" -> sendMessage(chatId, remoteControl.dogHandlerList());
 //                                                                                          for cats
                     case "/cat_rules" -> sendMessage(chatId, remoteControl.catRules());
-//
 //                                                                                          Menu Command from Point#3
                     case "/daily_report_form" -> sendMessage(chatId, remoteControl.dailyReportForm());
 //                                                                                      ...
@@ -86,6 +98,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                     case "/health_and_safety" -> sendMessage(chatId, remoteControl.heathAndSafety());
 
                     case "/client_contacts" -> sendMessage(chatId, remoteControl.clientContacts());
+                    case "/firstname" -> sendMessage(chatId, "Введите имя в формате: firstname Имя");
+                    case "/surname" -> sendMessage(chatId, "Введите отчество в формате: surname Отчество");
+                    case "/lastname" -> sendMessage(chatId, "Введите фамилию в формате: lastname Фамилия");
+                    case "/phone" -> sendMessage(chatId,
+                            "Введите номер контактного телефона в формате: +7 999 999 99-99");
 //                                                                                          No such command
                     default -> sendMessage(chatId, "такой команды не существует. Вы можете вызвать справку" +
                             " командой /help");
@@ -98,27 +115,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private void sendMessage(Long chatId, String text) {
         SendMessage message = new SendMessage(chatId, text);
         SendResponse response = telegramBot.execute(message);
-    }
-
-    private void saveUser(Update update) {
-        if (userRepository.findByChatId(update.message().chat().id()) != null) {
-            return;
-        }
-
-        User user = update.message().from();
-        UserEntity userEntity = new UserEntity();
-
-        userEntity.setFirstname(user.firstName());
-        userEntity.setLastname(user.lastName());
-        userEntity.setChatId(update.message().chat().id());
-        userEntity.setRegistrationDate(LocalDateTime.now());
-        userEntity.setType(Type.NEW_CLIENT);
-        userEntity.setLogin(user.username());
-
-        userEntity.setSurname("asdgasdg");
-        userEntity.setPhone("asdgasg");
-
-        userRepository.save(userEntity);
     }
 
 }
